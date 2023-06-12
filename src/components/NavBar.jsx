df3cbecf66b4640be49f8cbd06c690a243e70d16
Your Name <you@example.com>
2023-06-12 16:11:04 +0300

    added search bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <div className='border
     flex items-center space-x-8 pl-12 py-4'>
-        <img className='w-[85px]' src={Logo} />
+        <Link to="/"><img className='w-[85px]' src={Logo} /></Link>
         <Link to="/" className='text-blue-400 font-bold text-xl'>Movies</Link>
         <Link to="/fav" className='text-blue-400 font-bold text-xl'>Favourites</Link>
     </div>
